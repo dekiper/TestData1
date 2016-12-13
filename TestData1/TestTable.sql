30c1183c71d74048ca04f11a0db72a8d3aeb6a05
dekiper <dekiper@DEJAN-LAPTOP>
2016-12-13 22:01:02 +0100

    Dodat aktivan

--- TestData1/TestTable.sql
+++ TestData1/TestTable.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Prezime] NVARCHAR(50) NULL, 
     [Ime] NVARCHAR(50) NULL, 
-    [Telefon] CHAR(10) NULL
+    [Telefon] CHAR(10) NULL, 
+    [Aktivan] BIT NULL
 )
